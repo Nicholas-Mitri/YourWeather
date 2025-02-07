bf812f8053d557901c090f5c3c5125b8bdf4410f
Nicholas Mitri <nicholas.g.mitri@gmail.com>
2025-02-06 19:36:32 -0500

    Add Flask route and home page for weather data API

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask("Website")
+
+
+@app.route("/")
+def home():
+    return render_template("/home.html")
+
+
+@app.route("/api/v1/<station>/<date>/")
+def about(station, date):
+    date = datetime.strptime(date, "%Y%m%d")
+    print(date)
+    return {"Station": station, "Date": date.strftime("%b %d, %Y"), "temperature": 16}
+
+
+app.run(debug=True)
